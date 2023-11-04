4f5317cd75fa233ece33685864e31c0cf8ad020e
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2023-11-04 08:25:04 +0530

    srvr script engin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,29 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Coupons from './pages/Coupons';
 import Cart from './pages/Cart';
+import Navbar from './components/Navbar';
+
+const NotFound = () => {
+  return (<>
+    <Navbar />
+    <div className="flex font-mono items-center justify-center h-screen  text-black">
+      <h1 className="sm:text-2xl">404 | Page Not Found</h1>
+    </div>
+  </>
+  );
+};
 
 function App() {
-
-  return (<>
-    <Routes>
-      <Route path="/" element={<Coupons />} />
-      <Route path="/cart" element={<Cart />} />
-    </Routes>
-  </>
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Coupons />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
 export default App;
+
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
-import { BiSolidHot, BiChevronDown, BiSolidUser, BiSolidCartAlt, BiMenu } from 'react-icons/bi';
-import { GrClose } from 'react-icons/gr'
+import { Link, useLocation } from 'react-router-dom';
+import { BiSolidHot, BiChevronDown, BiChevronUp, BiSolidUser, BiSolidCartAlt, BiMenu } from 'react-icons/bi';
+import { GrClose } from 'react-icons/gr';
+
 const Navbar = () => {
+    const location = useLocation();
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
     const toggleMobileMenu = () => {
@@ -14,24 +17,24 @@ const Navbar = () => {
 
     return (
         <div className="bg-yellow-100">
-            <div className="flex  font-mono flex-row justify-between  mx-10   ">
+            <div className="flex font-mono flex-row justify-between mx-10">
                 <div className="flex justify-center items-center gap-1 px-2 text-xl">
                     <BiSolidHot />
                     <h1> Hotbeans</h1>
                 </div>
 
-                <div className="flex justify-between gap-2 text-sm md:gap-8 items-center  pt-4 p-2   px-1">
-
-                    <div className="hidden sm:flex justify-between gap-2 text-sm md:gap-8 items-center     px-1">
-                        <button className="flex justify-center items-center mx-2 text-md">
+                <div className="flex justify-between gap-2 text-sm md:gap-8 items-center pt-4 p-2 px-1">
+                    <div className="hidden sm:flex justify-between gap-2 text-sm md:gap-8 items-center px-1">
+                        <Link to="/" className={`flex justify-center items-center mx-2 text-md ${location.pathname === '/' ? 'text-red-600' : ''}`}>
                             Bestsellers
-                        </button>
-                        <button className="flex justify-center items-center text-md gap-1">
-                            Shop all <BiChevronDown className="flex text-2xl items-center" />
-                        </button>
-                        <button className="flex justify-center items-center text-md gap-1">
-                            % Sales <BiChevronDown className="flex text-2xl items-center" />
-                        </button>
+                        </Link>
+                        <Link to="/shope" className={`flex justify-center items-center text-md gap-1 ${location.pathname === '/shope' ? 'text-red-600' : ''}`}>
+                            Shop all {location.pathname === '/shope' ? (<BiChevronUp className="flex text-2xl items-center" />) : (<BiChevronDown className="flex text-2xl items-center" />)}
+                        </Link>
+                        <Link to="/seller" className={`flex justify-center items-center text-md gap-1 ${location.pathname === '/seller' ? 'text-red-600' : ''}`}>
+                            % Sales  {location.pathname === '/seller' ? (<BiChevronUp className="flex text-2xl items-center" />) : (<BiChevronDown className="flex text-2xl items-center" />)}
+
+                        </Link>
                         <div className="flex justify-center items-center text-md gap-2">
                             <BiSolidUser className="text-xl" />
                             <h2>Hi kiran</h2>
@@ -39,10 +42,10 @@ const Navbar = () => {
                     </div>
 
                     <div className="relative flex items-center">
-                        <div className="flex justify-center text-3xl gap-1 cursor-pointer">
-                            <BiSolidCartAlt className="z-10" />
+                        <Link to="/cart" className="flex justify-center text-3xl gap-1 cursor-pointer">
+                            <BiSolidCartAlt className={`z-10 ${location.pathname === '/cart' ? 'text-red-600' : ''}`} />
                             <span
-                                className="absolute top right-1 bg-yellow-400 mr-1 text-white p-2 rounded-full  text-xs "
+                                className="absolute top right-1 bg-yellow-400 mr-1 text-white p-2 rounded-full text-xs"
                                 style={{
                                     transform: 'translate(50%, -50%)',
                                     width: '1.2rem',
@@ -51,33 +54,31 @@ const Navbar = () => {
                                     justifyContent: 'center',
                                     alignItems: 'center',
                                 }}
-                            > 3
+                            >
+                                3
                             </span>
-                        </div>
+                        </Link>
                     </div>
                     <BiMenu className="flex sm:hidden justify-center text-3xl gap-1 cursor-pointer" onClick={toggleMobileMenu} />
                 </div>
-
-
             </div>
-            {isMobileMenuOpen && (<div>
-
-
-                <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-blue-500 bg-opacity-95 p-5">
-                    <div className="bg-white flex  flex-col justify-start items-start  rounded-lg px-6  gap-3 py-10">
-                        <button className="text-lg  rounded-lg px-2 hover:bg-yellow-50 hover:text-yellow-600" onClick={closeMobileMenu}>
-                            Bestsellers
-                        </button>
-                        <button className="text-lg  rounded-lg px-2 hover:bg-yellow-50 hover:text-yellow-600" onClick={closeMobileMenu}>
-                            Shop all
-                        </button>
-                        <button className="text-lg  rounded-lg px-2 hover:bg-yellow-50 hover:text-yellow-600" onClick={closeMobileMenu}>
-                            % Sales
-                        </button>
+            {isMobileMenuOpen && (
+                <div>
+                    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-blue-500 bg-opacity-95 p-5">
+                        <div className="bg-white flex flex-col justify-start items-start rounded-lg px-6 gap-3 py-10">
+                            <Link to="/" className={`text-lg rounded-lg px-2 hover:bg-yellow-50 hover:text-yellow-600  ${location.pathname === '/' ? 'text-red-600' : ''}`} onClick={closeMobileMenu}>
+                                Bestsellers
+                            </Link>
+                            <Link to="/shope" className={`text-lg rounded-lg px-2 hover:bg-yellow-50 hover:text-yellow-600  ${location.pathname === '/shope' ? 'text-red-600' : ''}`} onClick={closeMobileMenu}>
+                                Shop all
+                            </Link>
+                            <Link to="/seller" className={`text-lg rounded-lg px-2 hover:bg-yellow-50 hover:text-yellow-600  ${location.pathname === '/seller' ? 'text-red-600' : ''}`} onClick={closeMobileMenu}>
+                                % Sales
+                            </Link>
+                        </div>
                     </div>
+                    <GrClose className="fixed top-3 right-3 text-2xl" onClick={closeMobileMenu} />
                 </div>
-                <GrClose className="fixed top-3 right-3 text-2xl  " onClick={closeMobileMenu} />
-            </div>
             )}
         </div>
     );
